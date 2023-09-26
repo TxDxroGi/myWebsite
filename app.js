72f542d85f03f6808245e7f60fa859fbbcee295d
Pxwadol KxP <127196189+TxDxroGi@users.noreply.github.com>
2023-09-26 15:33:48 +0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+let menuToggle = document.querySelector('.menuToggle');
+let sidebar = document.querySelector('sidebar');
+menuToggle.onclick = function(){
+    menuToggle.classList.toggle('active');
+    sidebar.classList.toggle('active');
+}
