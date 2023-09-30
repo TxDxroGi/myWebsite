90d56db5bdd63c68a50bd896d0da5961c1a3383c
Pxwadol KxP <127196189+TxDxroGi@users.noreply.github.com>
2023-09-30 19:39:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ gsap.to(".w1" , {
         start: "top 80%",
         end: "top 30%",
         scrub: 5,
-        toggleActions: "restart pause none none",
+        toggleActions: "restart pause restart none",
         // play pause resume reverse restart reset comolete none
         //         onEnter | onLeave | onEnterBack | onLeaveBack  
         markers: {
