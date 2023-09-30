3ab6792bba6e82c395234d312543e861582c1cb9
Pxwadol KxP <127196189+TxDxroGi@users.noreply.github.com>
2023-09-30 16:21:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 gsap.registerPlugin(ScrollTrigger);
 
+TweenMax.to('.multitext', 2, {
+    delay: .8,
+    width: '50%',
+    ease: Power2.easeInOut
+})
+
 gsap.to(".w1" , {
-    x: -650,
-    duration: 4,
-    scrollTrigger: ".w2"
+    x: -700,
+    duration: 8,
+    scrollTrigger: {
+        trigger: ".w1",
+        start: "top 80%",
+        end: "top 30%",
+        scrub: 5,
+        toggleActions: "restart none none none",
+        // play pause resume reverse restart reset comolete none
+        //         onEnter | onLeave | onEnterBack | onLeaveBack  
+        markers: {
+            startColor: "purple",
+            endColor: "fuchsia",
+            fontSize: "1rem",
+    },
+  }
 })
