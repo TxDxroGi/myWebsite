ea79edcb1d3170ba9662fbbed3fd68a57fc12d17
Pxwadol KxP <127196189+TxDxroGi@users.noreply.github.com>
2023-10-03 09:59:40 +0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".animate-text", {
+    x: -900,
+    duration: 30,
+    scrollTrigger: {
+        trigger: ".bg-text-container",
+        start: "top 80%",
+        end: "top 2%",
+        scrub: 8,
+        markers: {
+            startsColor: "purple",
+            endColor: "fuchsia",
+            fontSize: "1rem",
+        }
+    }
+})
+gsap.to(".animate-text.left", {
+    x: 900,
+    duration: 30,
+    scrollTrigger: {
+        trigger: ".bg-text-container",
+        start: "top 80%",
+        end: "top 2%",
+        scrub: 8,
+        markers: {
+            startsColor: "purple",
+            endColor: "fuchsia",
+            fontSize: "1rem",
+        }
+    }
+})
