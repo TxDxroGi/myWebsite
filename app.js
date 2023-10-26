8c370cc544cc480d2e92236bda9ac9c8350f2c3e
Pxwadol KxP <127196189+TxDxroGi@users.noreply.github.com>
2023-10-27 06:17:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,24 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".animate-text", {
-    x: -900,
-    duration: 30,
+
+gsap.config({ trialWarn: false });
+console.clear();
+gsap.registerPlugin(ScrollTrigger, SplitText);
+const split = new SplitText("p", { type: "lines" });
+
+split.lines.forEach((target) => {
+  gsap.to(target, {
+    backgroundPositionX: 0,
+    ease: "none",
     scrollTrigger: {
-        trigger: ".bg-text-container",
-        start: "top 80%",
-        end: "top 2%",
-        scrub: 8,
-        // markers: {
-        //     startsColor: "purple",
-        //     endColor: "fuchsia",
-        //     fontSize: "1rem",
-        // }
+      trigger: target,
+      markers: true,
+      scrub: 1,
+      start: "top center",
+      end: "bottom center"
     }
-})
-gsap.to(".animate-text.left", {
-    x: 900,
-    duration: 30,
-    scrollTrigger: {
-        trigger: ".bg-text-container",
-        start: "top 80%",
-        end: "top 2%",
-        scrub: 8,
-        // markers: {
-        //     startsColor: "purple",
-        //     endColor: "fuchsia",
-        //     fontSize: "1rem",
-        // }
-    }
-})
+  });
+});
 
 // var words = ['console.log("my-portfolio");', 'puts "I am old 16 years old"', 
 // 'console.log("I want to be a programmer")','echo "I want to write code";'],
